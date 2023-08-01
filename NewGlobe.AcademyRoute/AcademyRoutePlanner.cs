ee4868b5dffccf8016c6834736a99809ad43fa55
Burak Celik <burak.celik@mentormate.com>
2023-08-01 15:48:03 +0300

    academy route implemented

---
+++ NewGlobe.AcademyRoute/AcademyRoutePlanner.cs
@@ -0,0 +1,105 @@
+﻿namespace NewGlobe.AcademyRoute
+{
+    public class AcademyRoutePlanner
+    {
+        private Dictionary<char, Dictionary<char, int>> routes;
+
+        public AcademyRoutePlanner()
+        {
+            routes = new Dictionary<char, Dictionary<char, int>>();
+        }
+
+        public void AddRoute(char start, char end, int distance)
+        {
+            if (!routes.ContainsKey(start))
+                routes[start] = new Dictionary<char, int>();
+
+            routes[start][end] = distance;
+        }
+
+        public int GetDistance(List<char> academies)
+        {
+            int distance = 0;
+            for (int i = 0; i < academies.Count - 1; i++)
+            {
+                if (!routes.ContainsKey(academies[i]) || !routes[academies[i]].ContainsKey(academies[i + 1]))
+                    return -1; // Invalid route, return -1
+                distance += routes[academies[i]][academies[i + 1]];
+            }
+            return distance;
+        }
+
+        public int CountRoutesWithMaxStops(char start, char end, int maxStops, int currentStops = 0)
+        {
+            if (currentStops > maxStops)
+                return 0;
+
+            if (currentStops > 0 && start == end)
+                return 1;
+
+            if (!routes.ContainsKey(start))
+                return 0;
+
+            int count = 0;
+            foreach (var neighbor in routes[start])
+            {
+                count += CountRoutesWithMaxStops(neighbor.Key, end, maxStops, currentStops + 1);
+            }
+            return count;
+        }
+
+        public int CountRoutesWithExactStops(char start, char end, int exactStops, int currentStops = 0)
+        {
+            if (currentStops == exactStops && start == end)
+                return 1;
+
+            if (currentStops >= exactStops)
+                return 0;
+
+            if (!routes.ContainsKey(start))
+                return 0;
+
+            int count = 0;
+            foreach (var neighbor in routes[start])
+            {
+                count += CountRoutesWithExactStops(neighbor.Key, end, exactStops, currentStops + 1);
+            }
+            return count;
+        }
+
+        public int ShortestRoute(char start, char end, int currentDistance = 0, int shortestDistance = int.MaxValue)
+        {
+            if (!routes.ContainsKey(start))
+                return int.MaxValue;
+
+            if (start == end)
+                return currentDistance;
+
+            foreach (var neighbor in routes[start])
+            {
+                int newDistance = ShortestRoute(neighbor.Key, end, currentDistance + neighbor.Value, shortestDistance);
+                shortestDistance = Math.Min(shortestDistance, newDistance);
+            }
+            return shortestDistance == int.MaxValue ? -1 : shortestDistance;
+        }
+
+        public int CountRoutesWithMaxDistance(char start, char end, int maxDistance, int currentDistance = 0)
+        {
+            if (currentDistance >= maxDistance)
+                return 0;
+
+            if (start == end && currentDistance > 0)
+                return 1;
+
+            if (!routes.ContainsKey(start))
+                return 0;
+
+            int count = 0;
+            foreach (var neighbor in routes[start])
+            {
+                count += CountRoutesWithMaxDistance(neighbor.Key, end, maxDistance, currentDistance + neighbor.Value);
+            }
+            return count;
+        }
+    }
+}
--- NewGlobe.AcademyRoute/Program.cs
+++ NewGlobe.AcademyRoute/Program.cs
@@ -6,7 +6,31 @@ namespace NewGlobe.AcademyRoute
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AcademyRoutePlanner planner = new AcademyRoutePlanner();
+
+            // Test Input: AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
+            planner.AddRoute('A', 'B', 5);
+            planner.AddRoute('B', 'C', 4);
+            planner.AddRoute('C', 'D', 8);
+            planner.AddRoute('D', 'C', 8);
+            planner.AddRoute('D', 'E', 6);
+            planner.AddRoute('A', 'D', 5);
+            planner.AddRoute('C', 'E', 2);
+            planner.AddRoute('E', 'B', 3);
+            planner.AddRoute('A', 'E', 7);
+
+            Console.WriteLine("1. Distance of the route A-B-C: " + FormatDistanceOutput(planner.GetDistance(new List<char> { 'A', 'B', 'C' })));
+            Console.WriteLine("2. Distance of the route A-E-B-C-D: " + FormatDistanceOutput(planner.GetDistance(new List<char> { 'A', 'E', 'B', 'C', 'D' })));
+            Console.WriteLine("3. Distance of the route A-E-D: " + FormatDistanceOutput(planner.GetDistance(new List<char> { 'A', 'E', 'D' })));
+            Console.WriteLine("4. Number of trips starting at C and ending at C with a maximum of 3 stops: " + planner.CountRoutesWithMaxStops('C', 'C', 3));
+            Console.WriteLine("5. Number of trips starting at A and ending at C with exactly 4 stops: " + planner.CountRoutesWithExactStops('A', 'C', 4));
+            Console.WriteLine("6. Length of the shortest route from A to C: " + FormatDistanceOutput(planner.ShortestRoute('A', 'C')));
+            Console.WriteLine("7. Length of the shortest route from B to B: " + FormatDistanceOutput(planner.ShortestRoute('B', 'B')));
+            Console.WriteLine("8. Number of different routes from C to C with a distance of less than 30: " + planner.CountRoutesWithMaxDistance('C', 'C', 30));
+        }
+        private static string FormatDistanceOutput(int distance)
+        {
+            return distance >= 0 ? distance.ToString() : "NO SUCH ROUTE";
         }
     }
 }
